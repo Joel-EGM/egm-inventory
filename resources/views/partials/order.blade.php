1ecca46aaff963281963666e28424884eecb59fa
Joel-EGM <111558904+Joel-EGM@users.noreply.github.com>
2022-12-22 16:35:52 +0800

    addedValue

--- resources/views/partials/order.blade.php
+++ resources/views/partials/order.blade.php
@@ -51,7 +51,7 @@
             class="block appearance-none w-full bg-white border border-gray-400
                 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow 
                 leading-tight focus:outline-none focus:shadow-outline">
-            <option value="" class="text-center text-gray-400">--select item--</option>
+            <option value="None" class="text-center text-gray-400">--select item--</option>
 
             @foreach ($itemList as $item)
                 <option value="{{ $item->id }}" class="text-center">{{ $item->item_name }}
@@ -69,7 +69,7 @@
             class="block appearance-none w-full bg-white border border-gray-400
              hover:border-gray-500 px-4 py-2 pr-8 rounded shadow 
              leading-tight focus:outline-none focus:shadow-outline">
-            <option value="" class="text-center text-gray-400">--select unit--</option>
+            <option value="None" class="text-center text-gray-400">--select unit--</option>
 
             @foreach ($unitName as $unit)
                 <option class="w-full text-center" value="{{ $unit->id }}">{{ $unit->unit_name }}</option>
