27b42b92feb93f7374f76ead14cad2dcfa4aa123
Joel-EGM <111558904+Joel-EGM@users.noreply.github.com>
2023-01-20 13:58:37 +0800

    addUserRolePermission

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,98 +16,100 @@
                             {{ __('Dashboard') }}
                         </x-jet-nav-link>
 
+                        @can('isAdmin')
+                            <!-- Stocks DropDown List -->
+                            <x-jet-dropdown align="right" width="48" class="bg-gray-800">
+                                <x-slot name="trigger">
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white-300 hover:bg-gray-700 hover:text-white focus:bg-gray-400 focus:outline-none transition ease-in-out duration-150">
+                                            {{ __('Stocks') }}
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </x-slot>
+                                <x-slot name="content">
 
-                        <!-- Stocks DropDown List -->
-                        <x-jet-dropdown align="right" width="48" class="bg-gray-800">
-                            <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <button type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white-300 hover:bg-gray-700 hover:text-white focus:bg-gray-400 focus:outline-none transition ease-in-out duration-150">
-                                        {{ __('Stocks') }}
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </x-slot>
-                            <x-slot name="content">
+                                    <x-jet-dropdown-link href="{{ route('stocks') }}">
+                                        {{ __('Current Stock') }}
+                                    </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="#">
+                                        {{ __('Track Usage') }}
+                                    </x-jet-dropdown-link>
 
-                                <x-jet-dropdown-link href="{{ route('stocks') }}">
-                                    {{ __('Current Stock') }}
-                                </x-jet-dropdown-link>
-                                <x-jet-dropdown-link href="#">
-                                    {{ __('Track Usage') }}
-                                </x-jet-dropdown-link>
-
-                            </x-slot>
-                        </x-jet-dropdown>
+                                </x-slot>
+                            </x-jet-dropdown>
 
 
-                        <!-- Manage DropDown List -->
-                        <x-jet-dropdown align="right" width="48" class="bg-gray-800">
-                            <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <button type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white-300 hover:bg-gray-700 hover:text-white focus:bg-gray-400 focus:outline-none transition ease-in-out duration-150">
-                                        {{ __('Manage') }}
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </x-slot>
-                            <x-slot name="content">
+                            <!-- Manage DropDown List -->
+                            <x-jet-dropdown align="right" width="48" class="bg-gray-800">
+                                <x-slot name="trigger">
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white-300 hover:bg-gray-700 hover:text-white focus:bg-gray-400 focus:outline-none transition ease-in-out duration-150">
+                                            {{ __('Manage') }}
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </x-slot>
+                                <x-slot name="content">
 
-                                <x-jet-dropdown-link href="{{ route('branches') }}">
-                                    {{ __('Branch') }}
-                                </x-jet-dropdown-link>
-                                <x-jet-dropdown-link href="{{ route('suppliers') }}">
-                                    {{ __('Supplier') }}
-                                </x-jet-dropdown-link>
-                                <x-jet-dropdown-link href="{{ route('items') }}">
-                                    {{ __('Items') }}
-                                </x-jet-dropdown-link>
-                                <x-jet-dropdown-link href="{{ route('prices') }}">
-                                    {{ __('Pricing') }}
-                                </x-jet-dropdown-link>
-                                <x-jet-dropdown-link href="{{ route('users') }}">
-                                    {{ __('User') }}
-                                </x-jet-dropdown-link>
-                            </x-slot>
-                        </x-jet-dropdown>
+                                    <x-jet-dropdown-link href="{{ route('branches') }}">
+                                        {{ __('Branch') }}
+                                    </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="{{ route('suppliers') }}">
+                                        {{ __('Supplier') }}
+                                    </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="{{ route('items') }}">
+                                        {{ __('Items') }}
+                                    </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="{{ route('prices') }}">
+                                        {{ __('Pricing') }}
+                                    </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="{{ route('users') }}">
+                                        {{ __('User') }}
+                                    </x-jet-dropdown-link>
+                                </x-slot>
+                            </x-jet-dropdown>
+                        @endcan
+                        @canany(['isAdmin', 'isUser'])
+                            <!-- Orders DropDown List -->
+                            <x-jet-dropdown align="right" width="48" class="bg-gray-800">
+                                <x-slot name="trigger">
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white-300 hover:bg-gray-700 hover:text-white focus:bg-gray-400 focus:outline-none transition ease-in-out duration-150">
+                                            {{ __('Orders') }}
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </x-slot>
+                                <x-slot name="content">
 
-                        <!-- Orders DropDown List -->
-                        <x-jet-dropdown align="right" width="48" class="bg-gray-800">
-                            <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <button type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white-300 hover:bg-gray-700 hover:text-white focus:bg-gray-400 focus:outline-none transition ease-in-out duration-150">
-                                        {{ __('Orders') }}
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </x-slot>
-                            <x-slot name="content">
-
-                                <x-jet-dropdown-link href="{{ route('orders') }}">
-                                    {{ __('Create Order') }}
-                                </x-jet-dropdown-link>
-                                <x-jet-dropdown-link href="#">
-                                    {{ __('Order History') }}
-                                </x-jet-dropdown-link>
-                            </x-slot>
-                        </x-jet-dropdown>
+                                    <x-jet-dropdown-link href="{{ route('orders') }}">
+                                        {{ __('Create Order') }}
+                                    </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="#">
+                                        {{ __('Order History') }}
+                                    </x-jet-dropdown-link>
+                                </x-slot>
+                            </x-jet-dropdown>
+                        @endcanany
                     </div>
                 </div>
             </div>
