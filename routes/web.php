5b1901c48606c8fff997c2e804cf9592e949c454
Joel-EGM <111558904+Joel-EGM@users.noreply.github.com>
2023-02-13 12:26:00 +0800

    code clean up

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,8 @@ Route::middleware([
 
     //Orders
     Route::get('orders/create_order', \App\Http\Livewire\WireOrder::class, 'render')->name('orders');
-});
 
-Route::get('generate-pdf/1', [PDFController::class, 'generatePDF'])->name('generate-pdf1');
-Route::get('generate-pdf/2', [PDFController::class, 'generatePDF'])->name('generate-pdf2');
+    //EXPORT TO PDF
+    Route::get('generate-pdf/1', [PDFController::class, 'generatePDF'])->name('generate-pdf1');
+    Route::get('generate-pdf/2', [PDFController::class, 'generatePDF'])->name('generate-pdf2');
+});
