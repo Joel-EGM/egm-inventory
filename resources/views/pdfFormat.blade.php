63cee16a37f1434987954df230481df6c202f06c
Joel-EGM <111558904+Joel-EGM@users.noreply.github.com>
2023-05-02 10:36:31 +0800

    redesign format

--- resources/views/pdfFormat.blade.php
+++ resources/views/pdfFormat.blade.php
@@ -2,12 +2,45 @@
 <html>
 
 <head>
+    <style type="text/css">
+        @page {
+            margin: 0px;
+        }
+
+        body {
+            margin: 0px;
+        }
+
+        * {
+            font-family: Verdana, Arial, sans-serif;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        table {
+            font-size: x-small;
+        }
+
+        .stocks table {
+            margin: 15px;
+            text-align: center;
+
+        }
+
+        .stocks h3 {
+            margin-left: 15px;
+            text-align: center;
+        }
+    </style>
 
 </head>
 
 <body>
-    <img src="{{ public_path('/images/egmbanner.png') }}" alt="" style="width: 200px; height: 200px" />
-    <table class="table table-bordered">
+    {{-- <img src="{{ public_path('/images/egmbanner.png') }}" alt="" style="width: 200px; height: 200px" /> --}}
+    {{-- <table class="table table-bordered">
         <tr>
             <th>Item Name</th>
             <th>Unit Name</th>
@@ -26,8 +59,33 @@
                 <td>{{ $stock->created_at }}</td>
             </tr>
         @endforeach
-    </table>
+    </table> --}}
 
+    <div class="stocks">
+        <h3>INVENTORY STOCKS REPORT</h3>
+        <table width="100%">
+            <thead>
+                <tr>
+                    <th>Item Name</th>
+                    <th>Unit Name</th>
+                    <th>Whole</th>
+                    <th>Pieces</th>
+                    <th>Last Updated</th>
+                </tr>
+            </thead>
+
+            @foreach ($stocks as $stock)
+                <tr>
+                    <td>{{ $stock->item_name }}</td>
+                    <td>{{ $stock->unit_name }}</td>
+                    <td>{{ intval($stock->totalqtyWHOLE) }}</td>
+                    <td>{{ $stock->totalqtyREMAINDER }}</td>
+                    <td>{{ $stock->created_at }}</td>
+                </tr>
+            @endforeach
+
+        </table>
+    </div>
 </body>
 
 </html>
--- resources/views/pdfFormat2.blade.php
+++ resources/views/pdfFormat2.blade.php
@@ -2,27 +2,60 @@
 <html>
 
 <head>
+    <style type="text/css">
+        @page {
+            margin: 0px;
+        }
 
+        body {
+            margin: 0px;
+        }
+
+        * {
+            font-family: Verdana, Arial, sans-serif;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        table {
+            font-size: x-small;
+        }
+
+        .stocks table {
+            margin: 15px;
+            text-align: center;
+
+        }
+
+        .stocks h3 {
+            margin-left: 15px;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
-    <img src="{{ public_path('/images/egmbanner.png') }}" alt="" style="width: 200px; height: 200px" />
-    <table class="table table-bordered">
-        <tr>
-            <th>Item Name</th>
-            <th>Quantity</th>
-            <th>Date Received</th>
-
-
-        </tr>
-        @foreach ($stocks as $stock)
-            <tr>
-                <td>{{ $stock->item_name }}</td>
-                <td>{{ $stock->totalqty }}</td>
-                <td>{{ $stock->created_at }}</td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="stocks">
+        <h3>INVENTORY STOCKS REPORT</h3>
+        <table width="100%">
+            <thead>
+                <tr>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
+                    <th>Date Received</th>
+                </tr>
+            </thead>
+            @foreach ($stocks as $stock)
+                <tr>
+                    <td>{{ $stock->item_name }}</td>
+                    <td>{{ $stock->totalqty }}</td>
+                    <td>{{ $stock->created_at }}</td>
+                </tr>
+            @endforeach
+        </table>
 
 </body>
 
